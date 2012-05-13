a9e0b85a4ef6a73951d8d169ebe2c0ff683c153d
Scott Byrns <contact@scottbyrns.com>
2012-05-12 23:07:18 -1000

    Making object properties private.

--- typesafe.js
+++ typesafe.js
@@ -16,48 +16,50 @@
 
 __GLOBAL_NAMESPACE__ = window;
 
-Object.prototype.setType = function (type) {
-	this.objectType = type;
+Object.prototype.__setType = function (type) {
+	this.__objectType__ = type;
 	return this;
 };
 
-Object.prototype.checkType = function (object) {
+Object.prototype.__checkType = function (object) {
 	var type = object;
-	if (type == this.objectType) {
+	if (type == this.__objectType__) {
 		return this;
 	}
-	throw new Error("Object of type: " + type + " found, expected object of type: " + this.objectType);
+	throw new Error("Object of type: " + type + " found, expected object of type: " + this.__objectType__);
 };
 
-Object.prototype.getType = function () {
-	if (this.objectType === "undefined") {
-		this.objectType = typeof this;
+Object.prototype.__getType = function () {
+	if (this.__objectType__ === "undefined") {
+		this.__objectType__ = typeof this;
 	}
-	return this.objectType;
+	return this.__objectType__;
 };
 
 Function.prototype.checkType = function () {
 	for (var i = 0; i < arguments.length; i += 1) {
 		var type = (typeof arguments[i]);
 		try {
-			arguments[i].getType();
+			arguments[i].__getType();
 		}
 		catch (e) {
 			// No type probabbly a primative.
 		}
-		if (this.interface['input-parameters'][i].type != type) {
-			throw new Error("Expected value of type: " + this.interface['input-parameters'][i].type + " but found value of type: " + type + ' for argument named: ' + this.interface['input-parameters'][i].name);
+		if (this.__interface__['input-parameters']) {
+			if (this.__interface__['input-parameters'][i].type != type) {
+				throw new Error("Expected value of type: " + this.__interface__['input-parameters'][i].type + " but found value of type: " + type + ' for argument named: ' + this.__interface__['input-parameters'][i].name);
+			}	
 		}
 	}
 	return true;
 }
 
-Function.prototype.setInterface = Object.prototype.setInterface = function (interface) {
-	this.interface = interface;
+Function.prototype.__set_interface__ = Object.prototype.__set_interface__ = function (interface) {
+	this.__interface__ = interface;
 };
 
 Function.prototype.getInterface = Object.prototype.getInterface = function () {
-	return this.interface;
+	return this.__interface__;
 };
 
 
@@ -71,36 +73,36 @@ Function.prototype.isImplementationOf = Object.prototype.isImplementationOf = fu
 };
 
 Function.prototype.checkInterfaceAgainst = Object.prototype.checkInterfaceAgainst = function (object) {
-	if (this.interface == undefined && object && object.interface) {
+	if (this.__interface__ == undefined && object && object.__interface__) {
 		throw new Error("Interface was not defined.");
 	}
-	if (object.interface) {
-		if (object.interface['input-parameters'] && this.interface['input-parameters']) {
+	if (object.__interface__) {
+		if (object.__interface__['input-parameters'] && this.__interface__['input-parameters']) {
 			for (
-				var valueIndex = 0, valueLength = this.interface['input-parameters'].length;
+				var valueIndex = 0, valueLength = this.__interface__['input-parameters'].length;
 				valueIndex < valueLength;
 				valueIndex += 1
 			)
 			{
-				if (this.interface['return-value'].type != object.interface['return-value'].type) {
+				if (this.__interface__['return-value'].type != object.__interface__['return-value'].type) {
 					throw new Error([
 						"Provided object does not return a value of type: ",
-						this.interface['return-value'].type
+						this.__interface__['return-value'].type
 					].join(""));
 				}
-				if (this.interface['input-parameters'][valueIndex].name != object.interface['input-parameters'][valueIndex].name) {
+				if (this.__interface__['input-parameters'][valueIndex].name != object.__interface__['input-parameters'][valueIndex].name) {
 					throw new Error([
 						"Provided object does accept argument named: ",
-						this.interface['input-parameters'][valueIndex].name,
+						this.__interface__['input-parameters'][valueIndex].name,
 						'.'
 					].join(''));
 				}
-				if (this.interface['input-parameters'][valueIndex].type != object.interface['input-parameters'][valueIndex].type) {
+				if (this.__interface__['input-parameters'][valueIndex].type != object.__interface__['input-parameters'][valueIndex].type) {
 					throw new Error([
 						"Provided object does accept argument named: ",
-						this.interface['input-parameters'][valueIndex].name,
+						this.__interface__['input-parameters'][valueIndex].name,
 						' of type ',
-						this.interface['input-parameters'][valueIndex].type,
+						this.__interface__['input-parameters'][valueIndex].type,
 						'.'
 					].join(''));
 				}
@@ -132,49 +134,48 @@ var TypeSafeClass = function (global) {
 					throw new Error("No static constructor is available for this type safe class.");
 				}
 				try {
-					this.setType(config.type);
-					config.constructor.setInterface(Interface.getInterfaceNamed(config.implements).getConstructor());
+					this.__setType(config.type);
+					config.constructor.__set_interface__(Interface.getInterfaceNamed(config.implements).getConstructor());
 					config.constructor.checkType.apply(arguments);
 					config.constructor.apply(this, arguments);
-					this.setInterface(Interface.getInterfaceNamed(config.implements));
+					this.__set_interface__(Interface.getInterfaceNamed(config.implements));
 				}
 				catch (e) {
+					console.log(e);
 					throw new Error("No static constructor is available for this type safe class.")
 				}
-				this.getAccessControlList = function (internalCall) {
-					if (internalCall) {
-						
-					}
+				this.__get_access_control_list__ = function (internalCall) {
 					return methodAccessControlList;
 				}
-			}
+			};
 		}(config);
-		theClass.setInterface(Interface.getInterfaceNamed(config.implements));
+		theClass.__set_interface__(Interface.getInterfaceNamed(config.implements));
 
 		var properties = config.properties;
+		var privateProperties = {};
 		for (propertyIndex in properties) {
 			if (properties.hasOwnProperty(propertyIndex)) {
 				var txt = properties[propertyIndex].name;
 				var type = properties[propertyIndex].type;
 				var camelCasedMethodName = txt.substr(0, 1).toUpperCase() + txt.substr(1, txt.length);
 				theClass.prototype['get' + camelCasedMethodName] = function () {
-					return this[txt];
+					return privateProperties[txt];
 				};
 				theClass.prototype['set' + camelCasedMethodName] = function (typedValue) {
 					var inputType = (typeof typedValue);
-					if (typedValue.getType && typedValue.getType() != undefined) {
-						inputType = typedValue.getType();						
+					if (typedValue.__getType && typedValue.__getType() != undefined) {
+						inputType = typedValue.__getType();						
 					}
 					try {
 						if (inputType == type) {
-							this[txt] = arguments[0];
+							privateProperties[txt] = arguments[0];
 						}
 						else {
 							throw new Error("Expected value of type: " + type + " but found value of type: " + inputType);
 						}
 					}
 					catch (e) {
-						throw new Error(e);
+						console.log(e);
 						throw new Error("Setting typed value with untyped parameter.");
 					}
 				};
@@ -190,24 +191,24 @@ var TypeSafeClass = function (global) {
 			var method = methods[methodId];
 			theClass.prototype[method.name] = function (method) {
 				if (config[method.name]) {
-					config[method.name].setInterface(method.interface);
+					config[method.name].__set_interface__(method.interface);
 					var operation = function () {
 						var allGo = config[method.name].checkType.apply(arguments);
-						if (this.getAccessControlList()[method.name].scope == 'private') {
-							if (this.uid != this.getAccessControlList()[method.name].uid) {
+						if (this.__get_access_control_list__()[method.name].scope == 'private') {
+							if (this.__access_token__ != this.__get_access_control_list__()[method.name].uid) {
 								throw new Error("Access violation. Call to private method.");
 							}
 						}
 						if (allGo) {
 							// Issue UID to our object.
-							this.uid = this.getAccessControlList()[method.name].uid;
+							this.__access_token__ = this.__get_access_control_list__()[method.name].uid;
 							var output = config[method.name].apply(this, arguments);
 							// Strip the UID now that our method call is finished.
-							this.uid = undefined;
+							this.__access_token__ = undefined;
 							return output;
 						}
 					};
-					operation.setInterface(method.interface);
+					operation.__set_interface__(method.interface);
 					return operation;
 				}
 				else {
@@ -237,7 +238,7 @@ var Interface = function () {
 
 	};
 	var Interface = function (config) {
-		this.setType(config.type);
+		this.__setType(config.type);
 		this.constructor = config.constructor;
 		this.methods = config.methods;
 		interfaceRegister[config.type] = this;
@@ -250,22 +251,32 @@ var Interface = function () {
 	Interface.prototype.getMethods = function () {
 		return this.methods;
 	};
-	
-	Interface.prototype.toXML = function () {
-		
-	};
-	
-	Interface.prototype.toJSON = function () {
 
-	};
-	
-	Interface.prototype.toJava = function () {
-		var outputObject = {
-			type: this.getType(),
-			constructor: this.constructor,
-			methods: this.methods
-		};
-	};
+	// 
+	// Interface.prototype.toXML = function () {
+	// 	
+	// };
+	// 
+	// Interface.prototype.toJSON = function () {
+	// 
+	// };
+	// 
+	// Interface.prototype.toJava = function () {
+	// 	var outputObject = {
+	// 		type: this.getType(),
+	// 		constructor: this.constructor,
+	// 		methods: this.methods
+	// 	};
+	// };
+	// 
+	// Interface.prototype.toObjC = function () {
+	// 	
+	// };
+	// 
+	// Interface.prototype.python = function () {
+	// 	
+	// };
+	// 
 
 	Interface.getInterfaceNamed = function (name) {
 		return interfaceRegister[name];
